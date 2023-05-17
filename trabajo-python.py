40a815228c10ab329501a7d4d6e5228609031903
eduaracw <133248780+eduaracw@users.noreply.github.com>
2023-05-17 08:00:22 -0500

    Create trabajo-python.py

---
+++ trabajo-python.py
@@ -0,0 +1,18 @@
+campeones = {
+    1930: "Uruguay",
+    1934: "Italia",
+    1938: "Italia",
+    1950: "Uruguay",
+    1954: "Alemania Occidental",
+    1958: "Brasil",
+    1962: "Brasil",
+    1966: "Inglaterra",
+    1970: "Brasil",
+    1974: "Alemania Occidental",
+    1978: "Argentina",
+    1982: "Italia",
+    1986: "Argentina"
+}
+
+for año, equipo in campeones.items():
+    print(f"Año {año}: {equipo}")
